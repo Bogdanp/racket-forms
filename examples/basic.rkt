575b9f708c0ea814dc44476cb0cff0f04b01607b
Bogdan Popa <popa.bogdanp@gmail.com>
2019-01-17 19:43:08 +0200

    chore(core): initial commit

---
+++ examples/basic.rkt
@@ -0,0 +1,38 @@
+#lang racket
+
+(require forms
+         racket/match
+         web-server/http
+         web-server/dispatch)
+
+(struct signup-data (username password))
+
+(define signup-form
+  (form*
+   ([username (~> text (required) (matches #rx".+@.+"))]
+    [password (~> text (required) (longer-than 8))])
+   (signup-data username password)))
+
+(define (render-signup-form render-widget)
+  `(form ((action "/")
+          (method "POST"))
+         (label "Username" ,(render-widget "username" (widget-text)))
+         ,@(render-widget "username" (widget-errors))
+         (label "Password" ,(render-widget "password" (widget-password)))
+         ,@(render-widget "password" (widget-errors))
+         (button ((type "submit")) "Sign up!")))
+
+(define (signup req)
+  (match (form-run signup-form req)
+    [(list 'passed data _)
+     (response/xexpr '(h1 "Signed up!"))]
+
+    [(list _ _ render-widget)
+     (response/xexpr (render-signup-form render-widget))]))
+
+(define-values (start reverse-uri)
+  (dispatch-rules
+   [("") #:method (or "get" "post") signup]))
+
+(module+ main
+  (serve/dispatch start))
---
+++ forms-lib/info.rkt
@@ -0,0 +1,6 @@
+#lang info
+
+(define collection 'multi)
+
+(define deps '("base" "web-server-lib"))
+(define build-deps '("rackunit" "web-server-lib"))
