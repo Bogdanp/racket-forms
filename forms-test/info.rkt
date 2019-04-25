30cb92bd59156493b44ac20e8e1b29fdb09b8f36
Bogdan Popa <bogdan@defn.io>
2019-04-25 22:49:01 +0300

    fix(build): add srfi-lite-lib dep to forms-test

--- forms-test/info.rkt
+++ forms-test/info.rkt
@@ -3,6 +3,10 @@
 (define collection 'multi)
 
 (define deps '())
-(define build-deps '("base" "forms-lib" "rackunit-lib" "web-server-lib"))
+(define build-deps '("base"
+                     "forms-lib"
+                     "rackunit-lib"
+                     "srfi-lite-lib"
+                     "web-server-lib"))
 
 (define update-implies '("forms-lib"))
