9caf8e39b3afc01e1e234efc751730428c11bd22
Bogdan Popa <bogdan@defn.io>
2019-10-11 12:11:24 +0300

    l10n: delay translations until the last moment

--- forms-lib/private/formlets.rkt
+++ forms-lib/private/formlets.rkt
@@ -38,47 +38,47 @@
       (f v)
       (ok v)))
 
-(define ((required #:message [message (translate 'err-required)]) v)
+(define ((required #:message [message #f]) v)
   (if v
       (ok v)
-      (err message)))
+      (err (or message (translate 'err-required)))))
 
-(define (matches p #:message [message (translate 'err-matches p)])
+(define (matches p #:message [message #f])
   (lift (lambda (v)
           (if (regexp-match? p v)
               (ok v)
-              (err message)))))
+              (err (or message (translate 'err-matches p)))))))
 
-(define (one-of pairs #:message [message (translate 'err-one-of (string-join (map (compose1 (curry format "~a") car) pairs) ", "))])
+(define (one-of pairs #:message [message #f])
   (lift (lambda (v)
           (cond
             [(findf (lambda (pair)
                       (equal? v (car pair))) pairs)
              => (compose1 ok cdr)]
 
-            [else (err message)]))))
+            [else (err (or message (translate 'err-one-of (string-join (map (compose1 (curry format "~a") car) pairs) ", "))))]))))
 
-(define (shorter-than n #:message [message (translate 'err-shorter-than (sub1 n))])
+(define (shorter-than n #:message [message #f])
   (lift (lambda (v)
           (if (< (string-length v) n)
               (ok v)
-              (err message)))))
+              (err (or message (translate 'err-shorter-than (sub1 n))))))))
 
-(define (longer-than n #:message [message (translate 'err-longer-than (add1 n))])
+(define (longer-than n #:message [message #f])
   (lift (lambda (v)
           (if (> (string-length v) n)
               (ok v)
-              (err message)))))
+              (err (or message (translate 'err-longer-than (add1 n))))))))
 
 (define (to-boolean v)
   (ok (not (not v))))
 
-(define (to-number #:message [message (translate 'err-to-number)])
+(define (to-number #:message [message #f])
   (lift (lambda (v)
           (define n (string->number v))
           (if n
               (ok n)
-              (err message)))))
+              (err (or message (translate 'err-to-number)))))))
 
 (define (to-symbol v)
   (ok (and v (string->symbol v))))
@@ -104,7 +104,10 @@
   (ensure binding/text to-boolean))
 
 (define binding/email
-  (ensure binding/text (matches #rx".+@.+" #:message (translate 'err-binding/email))))
+  (ensure binding/text
+          (lambda (v)
+            ;; delay execution s.t. the translation is done as late as possible.
+            ((matches #rx".+@.+" #:message (translate 'err-binding/email)) v))))
 
 (define binding/number
   (ensure binding/text (to-number)))
