a5459983f4c96ca2a0d3710dc421c3eabd4fc604
Bogdan Popa <bogdan@defn.io>
2020-08-08 18:02:53 +0300

    examples: clean up blog example

--- examples/blog.rkt
+++ examples/blog.rkt
@@ -1,123 +1,102 @@
-#lang racket
+#lang racket/base
 
 (require forms/base
-         net/base64
          racket/function
-         racket/random
+         racket/match
          web-server/dispatch
          web-server/http
-         web-server/http/redirect)
+         web-server/servlet/web)
+
+(struct author (name email))
+(struct article (author title slug content))
 
 (define (slugify title)
   (regexp-replace* #px"[^a-zA-Z0-9\\-_]" (string-downcase title) "-"))
-
-(struct author (name email))
 
 (define author-form
   (form* ([name (ensure binding/text (required) (longer-than 3) (shorter-than 50))]
           [email (ensure binding/email (required))])
     (author name email)))
 
-(struct article (author title slug content))
-
 (define article-form
   (form* ([author author-form]
           [title (ensure binding/text (required) (longer-than 1) (shorter-than 150))]
-          [content (ensure binding/text (required) (longer-than 1))])
+          [content (ensure binding/text (required) (longer-than 1))]
+          [_agreement (ensure binding/text (required #:message "You must check this box."))])
     (article author title (slugify title) content)))
 
-(define articles
+(define *articles*
   (box (list (article (author "Bogdan Popa" "bogdan@defn.io") "Part Two" "part-two" "Hi, it's me again!")
              (article (author "Bogdan Popa" "bogdan@defn.io") "Hello World!" "hello-world-" "Hi there, world!"))))
 
-(define (article-lookup slug)
-  (for/first ([article (unbox articles)]
+(define (find-article slug)
+  (for/first ([article (unbox *articles*)]
               #:when (string=? (article-slug article) slug))
     article))
 
-(define (article-add! article)
-  (box-swap! articles (lambda (articles)
-                        (cons article articles))))
+(define (add-article! new-article)
+  (box-swap! *articles* (lambda (articles)
+                          (cons new-article articles))))
 
-(define (article-replace! slug new-article)
-  (box-swap! articles (lambda (articles)
-                        (for/list ([article articles])
-                          (if (string=? (article-slug article) slug)
-                              new-article
-                              article)))))
+(define (replace-article! slug new-article)
+  (box-swap! *articles* (lambda (articles)
+                          (for/list ([article articles])
+                            (if (string=? (article-slug article) slug)
+                                new-article
+                                article)))))
 
-;; This is not that important for this example so you can ignore it.
-;; It atomically swaps the contents of a box.
-(define (box-swap! box f . args)
-  (let loop ()
-    (let* ([v0 (unbox box)]
-           [v1 (apply f v0 args)])
-      (if (box-cas! box v0 v1) v1 (loop)))))
+(define (box-swap! b f . args)
+  (let loop ([v (unbox b)])
+    (unless (box-cas! b v (apply f v args))
+      (loop (unbox b)))))
 
-;; Ditto for all the CSRF token stuff.  Since we're not using
-;; continuations to process the forms, we need CSRF tokens to validate
-;; that the user actually intended to make a request.
-(define csrf-token (make-parameter #f))
-
-(define (render-csrf-token)
-  `(input ((type "hidden") (name "csrf_token") (value ,(csrf-token)))))
-
-(define ((make-labeled-field label name widget) render-widget)
+(define ((labeled label [w (widget-text)]) name value errors)
   `(div
-    ((class "form__group"))
-    (label ,label ,(render-widget name widget))
-    ,@(render-widget name (widget-errors #:class "form__errors"))))
+    ([class "form__group"])
+    (label ,label " " ,(w name value errors))
+    ,@((widget-errors #:class "form__errors") name value errors)))
 
 (define (render-author-form render-widget)
-  (define render-name-field (make-labeled-field "Name" "name" (widget-text)))
-  (define render-email-field (make-labeled-field "Email" "email" (widget-email)))
-
   `(fieldset
-    ((class "form__fieldset"))
+    ([class "form__fieldset"])
     (legend "Author")
-    ,(render-name-field render-widget)
-    ,(render-email-field render-widget)))
+    ,(render-widget "name" (labeled "Name"))
+    ,(render-widget "email" (labeled "Email" (widget-email)))))
 
-(define (render-article-form render-widget [action-label "Save"])
-  (define author-form (render-author-form (widget-namespace "author" render-widget)))
-  (define render-title-field (make-labeled-field "Title" "title" (widget-text)))
-  (define render-content-field (make-labeled-field "Content" "content" (widget-textarea #:attributes '((columns "140")
-                                                                                                       (rows "5")))))
-  (define content-form `(fieldset
-                         ((class "form__fieldset"))
-                         (legend "Article")
-                         ,(render-title-field render-widget)
-                         ,(render-content-field render-widget)))
-
+(define (render-article-form target render-widget [action-label "Save"])
   `(form
-    ((action "")
-     (method "POST")
-     (class "form"))
-    ,author-form
-    ,content-form
-    ,(render-csrf-token)
-    (button ((type "submit")) ,action-label)))
+    ([action ,target]
+     [method "POST"]
+     [class "form"])
+    ,(render-author-form (widget-namespace "author" render-widget))
+    (fieldset
+     ([class "form__fieldset"])
+     (legend "Article")
+     ,(render-widget "title" (labeled "Title"))
+     ,(render-widget "content" (labeled "Content" (widget-textarea #:attributes '([columns "140"] [rows "5"]))))
+     ,(render-widget "_agreement" (labeled "I solemnly swear that I am up to no good" (widget-checkbox))))
+    (button ([type "submit"]) ,action-label)))
 
-(define (render-article article)
+(define (render-article the-article)
   `(div
-    ((class "article"))
+    ([class "article"])
     (h2
-     ((class "article__title"))
+     ([class "article__title"])
      (a
-      ((href ,(reverse-uri view-article (article-slug article))))
-      ,(article-title article)))
+      ([href ,(reverse-uri view-article (article-slug the-article))])
+      ,(article-title the-article)))
     (h5
-     ((class "article__subtitle"))
-     "Posted by " ,(author-name (article-author article)) " "
-     (a ((href ,(reverse-uri edit-article (article-slug article)))) "Edit"))
+     ([class "article__subtitle"])
+     "Posted by " ,(author-name (article-author the-article)) " "
+     (a ([href ,(reverse-uri edit-article (article-slug the-article))]) "Edit"))
     (div
-     ((class "article__content"))
-     ,(article-content article))))
+     ([class "article__content"])
+     ,(article-content the-article))))
 
 (define (render-template . content)
   (define (render-nav-item label uri)
     `(li
-      ((class "nav__items"))
+      ([class "nav__items"])
       (a ((href ,uri)) ,label)))
 
   (response/xexpr
@@ -127,13 +106,13 @@
       (title "Blag"))
      (body
       (div
-       ((class "nav"))
+       ([class "nav"])
        (ul
-        ((class "nav__items"))
+        ([class "nav__items"])
         ,(render-nav-item "Home" (reverse-uri home))
         ,(render-nav-item "New Article" (reverse-uri new-article))))
       (div
-       ((class "content"))
+       ([class "content"])
        ,@content)))))
 
 (define (render-not-found)
@@ -141,78 +120,64 @@
    '(h1 "Not Found")
    '(p "The page you are looking for does not exist.")))
 
-(define (home req)
-  (define all-articles (map render-article (unbox articles)))
+(define (home _req)
+  (define all-articles (map render-article (unbox *articles*)))
   (render-template
-   `(div ((class "articles")) ,@all-articles)))
+   `(div ([class "articles"]) ,@all-articles)))
 
 (define (new-article req)
-  (match (form-run article-form req)
-    [(list 'passed article _)
-     (article-add! article)
-     (redirect-to (reverse-uri view-article (article-slug article)))]
-
-    [(list _ _ render-widget)
-     (render-template
-      '(h1 "Add an Article")
-      (render-article-form render-widget "Add Article"))]))
-
-(define (view-article req slug)
-  (define article (article-lookup slug))
-  (cond
-    [article (render-template (render-article article))]
-    [else (render-not-found)]))
-
-(define (edit-article req slug)
-  (define article (article-lookup slug))
-  (cond
-    [article
-     (match (form-run article-form req #:defaults (hash "author.name" (author-name (article-author article))
-                                                        "author.email" (author-email (article-author article))
-                                                        "title" (article-title article)
-                                                        "content" (article-content article)) )
+  (send/suspend/dispatch
+   (lambda (embed/url)
+     (match (form-run article-form req)
        [(list 'passed article _)
-        (article-replace! slug article)
+        (add-article! article)
         (redirect-to (reverse-uri view-article (article-slug article)))]
 
        [(list _ _ render-widget)
         (render-template
-         '(h1 "Edit Article")
-         (render-article-form render-widget))])]
+         '(h1 "Add an Article")
+         (render-article-form (embed/url new-article) render-widget "Add Article"))]))))
 
-    [else (render-not-found)]))
+(define (view-article _req slug)
+  (cond
+    [(find-article slug)
+     => (lambda (the-article)
+          (render-template (render-article the-article)))]
+    [else
+     (render-not-found)]))
+
+(define (edit-article req slug)
+  (send/suspend/dispatch
+   (lambda (embed/url)
+     (cond
+       [(find-article slug)
+        => (match-lambda
+             [(article author title slug content)
+              (define defaults
+                (hash "author.name" (author-name author)
+                      "author.email" (author-email author)
+                      "title" title
+                      "content" content))
+
+              (match (form-run article-form req #:defaults defaults)
+                [(list 'passed new-article _)
+                 (replace-article! slug new-article)
+                 (redirect-to (reverse-uri edit-article slug))]
+
+                [(list _ _ render-widget)
+                 (render-template
+                  '(h1 "Edit Article")
+                  (render-article-form (embed/url (curryr edit-article slug)) render-widget))])])]
+
+       [else
+        (render-not-found)]))))
 
 (define-values (start reverse-uri)
   (dispatch-rules
    [("") home]
-   [("articles" "new") #:method (or "get" "post") new-article]
+   [("articles" "new") new-article]
    [("articles" (string-arg)) view-article]
-   [("articles" (string-arg) "edit") #:method (or "get" "post") edit-article]))
-
-(define ((csrf-protect handler) req . args)
-  (define csrf-cookie-name "csrf_token")
-  (define csrf-cookie (findf (lambda (cookie)
-                               (string=? csrf-cookie-name (client-cookie-name cookie)))
-                             (request-cookies req)))
-
-  (define csrf-value
-    (or (and csrf-cookie (client-cookie-value csrf-cookie))
-        (bytes->string/utf-8 (base64-encode (crypto-random-bytes 128) #""))))
-
-  (when (member (request-method req) '(#"DELETE" #"PATCH" #"POST" #"PUT"))
-    (define csrf-binding (bindings-assq #"csrf_token" (request-bindings/raw req)))
-    (when (not (and csrf-binding (string=? (bytes->string/utf-8 (binding:form-value csrf-binding)) csrf-value)))
-      (error 'csrf-protect)))
-
-  (define handler-response
-    (parameterize ([csrf-token csrf-value])
-      (apply handler req args)))
-
-  (if csrf-cookie
-      handler-response
-      (struct-copy response handler-response [headers (cons
-                                                       (cookie->header (make-cookie csrf-cookie-name csrf-value))
-                                                       (response-headers handler-response))])))
+   [("articles" (string-arg) "edit") edit-article]))
 
 (module+ main
-  (serve/dispatch (csrf-protect start)))
+  (serve/dispatch start))
